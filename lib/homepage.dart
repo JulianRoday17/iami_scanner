d51447442d90e9dd6722eac2ad9ca13d49d772c4
julianroday17 <julianroday17@gmail.com>
2022-12-02 13:16:53 +0700

    Small fix

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -39,7 +39,7 @@ class _HomePageState extends State<HomePage> {
         child: Text("Test"),
         onPressed: () async {
           print('Running command: ${ping.command}');
-          ping.;
+          ping;
         },
       )),
     );
